18030f770091cccd9513e79180aefeb203a0ab42
Virginia Dooley <vcdooley@gmail.com>
2020-08-31 17:09:25 +0100

    fix directory

---
+++ README.md
@@ -0,0 +1,25 @@
+**WORK IN PROGRESS: Activism toolkit**
+
+Take action and track your progress on issues that matter to you. 
+
+I would like to duplicate this app in React Native to make use of push notifications. 
+
+In the project directory, you can run:
+
+### `npm install`
+
+then 
+
+### `npm start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+This app uses the Google Sheets API for the backend database, built with node.js. 
+My intention was to make entering actions as simple as possible for non-technical users.
+
+The express server needs to run concurrently to the React app front end. 
+To run the node.js app and ensure your front end has access to data: 
+Open a new terminal window in the same directory and enter: ### `node index.js`
+
+
---
+++ index.js
@@ -0,0 +1,113 @@
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 1234;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+/**
+   * Get and store new token after prompting for user authorization, and then
+   * execute the given callback with the authorized OAuth2 client.
+   * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+   * @param {getEventsCallback} callback The callback for the authorized client.
+   */
+  function getNewToken(oAuth2Client, callback) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES,
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+      rl.close();
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) return console.error('Error while trying to retrieve access token', err);
+        oAuth2Client.setCredentials(token);
+        // Store the token to disk for later program executions
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+          if (err) return console.error(err);
+          console.log('Token stored to', TOKEN_PATH);
+        });
+        callback(oAuth2Client);
+      });
+    });
+  }
+
+  /**
+   * Prints the actions in the spreadsheet:
+   * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+   * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+   */
+
+   /**
+   * Create an OAuth2 client with the given credentials, and then execute the
+   * given callback function.
+   * @param {Object} credentials The authorization client credentials.
+   * @param {function} callback The callback to call with the authorized client.
+   */
+  function authorize(credentials, callback) {
+    const {client_secret, client_id, redirect_uris} = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+      if (err) return getNewToken(oAuth2Client, callback);
+      oAuth2Client.setCredentials(JSON.parse(token));
+      callback(oAuth2Client);
+    });
+  }
+
+app.get('/', (req, res) => {
+
+  // Load client secrets from a local file.
+  fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Sheets API.
+    authorize(JSON.parse(content), listActions);
+  });
+
+  function listActions(auth) {
+    const sheets = google.sheets({version: 'v4', auth});
+    sheets.spreadsheets.values.get({
+      spreadsheetId: '1HzqdR66JwQVAFegsVjtjwtc68lOld7c3kB4r589w5-4',
+      range: 'LevelOne!A1:Z100'
+
+    }, (err, response, sheets)=> {
+      if (err) return console.log('The API returned an error: ' + err);
+      
+      // (3) Setting data for daily tracking
+
+    let actions = response.data.values
+    let action_keys = actions.shift(); // remove actions[0]
+    let action_objects = actions.map(action => {
+    let obj = {};
+    action_keys.forEach((key, i) => obj[key] = action[i]);
+      return obj;
+    });
+    console.log(action_objects);
+      res.json({ actions: action_objects})
+    })
+  }
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
