455294956cbf6b8a408c772e1522ec40add23b21
Virginia Dooley <vcdooley@gmail.com>
2020-09-05 12:06:17 +0100

    Pass props to child component

--- src/ActionList.js
+++ src/ActionList.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 class ActionList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = this.props
+  }
+
     render() {
       return (
         <div>
-          Action List will eventually be passed into this component.
+          {this.props.actions.map((action, index) =>
+            <li key={index.toString()}>
+              <div className="uk-card uk-card-secondary uk-card-body uk-width-1-2@m">
+              <div className="uk-card-badge uk-label">{action.badge}</div>
+              <p className="uk-card-title">Action: {action.Title}</p>
+              <a href="{action.Url}" className="uk-link-muted">{action.Url_title}</a>
+              <p>Date added: {action.date_added}</p>
+              <p>Deadline: {action.deadline}</p>
+              <p id="status">Complete?: {action.Status}</p>
+              <button className="uk-button uk-button-primary" name={action} value={action.Status}>COMPLETE</button>
+              </div>
+            </li>
+          )}
         </div>
       )
     }
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      loading: "fetching data",
+      loading: "fetching actions...",
       actions: []
     }
   }
@@ -28,20 +28,7 @@ class App extends React.Component {
   render() {
     return (
         <ul className="uk-list">
-        <ActionList/>
-          {this.state.actions.map((action, index) =>
-            <li key={index.toString()}>
-              <div className="uk-card uk-card-secondary uk-card-body uk-width-1-2@m">
-              <div className="uk-card-badge uk-label">{action.badge}</div>
-              <p className="uk-card-title">Action: {action.Title}</p>
-              <a href="{action.Url}" className="uk-link-muted">{action.Url_title}</a>
-              <p>Date added: {action.date_added}</p>
-              <p>Deadline: {action.deadline}</p>
-              <p id="status">Complete?: {action.Status}</p>
-              <button className="uk-button uk-button-primary" name={action} value={action.Status}>COMPLETE</button>
-              </div>
-            </li>
-          )}
+          <ActionList actions={this.state.actions}/>
         </ul>
     );
   }
