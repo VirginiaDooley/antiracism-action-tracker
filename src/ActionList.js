4618ba6488aa3e129cda45a6da54cbdc4c642751
Virginia Dooley <vcdooley@gmail.com>
2020-09-06 21:14:32 +0100

    Refactor state and set up event listener

--- src/ActionList.js
+++ src/ActionList.js
@@ -3,28 +3,32 @@ import React from 'react';
 class ActionList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = this.props
+    this.state = {actions: this.props}
   }
 
-    render() {
-      return (
-        <div>
-          {this.props.actions.map((action, index) =>
-            <li key={index.toString()}>
-              <div className="uk-card uk-card-secondary uk-card-body uk-width-1-2@m">
-              <div className="uk-card-badge uk-label">{action.badge}</div>
-              <p className="uk-card-title">Action: {action.Title}</p>
-              <a href="{action.Url}" className="uk-link-muted">{action.Url_title}</a>
-              <p>Date added: {action.date_added}</p>
-              <p>Deadline: {action.deadline}</p>
-              <p id="status">Complete?: {action.Status}</p>
-              <button className="uk-button uk-button-primary" name={action} value={action.Status}>COMPLETE</button>
-              </div>
-            </li>
-          )}
-        </div>
-      )
-    }
+  toggleAction() {
+    console.log("I'm listening")
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.actions.map((action, index) =>
+          <li key={index.toString()}>
+            <div className="uk-card uk-card-secondary uk-card-body uk-width-1-2@m">
+            <div className="uk-card-badge uk-label">{action.badge}</div>
+            <p className="uk-card-title">Action: {action.Title}</p>
+            <a href="{action.Url}" className="uk-link-muted">{action.Url_title}</a>
+            <p>Date added: {action.date_added}</p>
+            <p>Deadline: {action.deadline}</p>
+            <p id="status">Complete?: {action.Status}</p>
+            <button className="uk-button uk-button-primary" name={action} value={action.Status} onClick={this.toggleAction}>COMPLETE</button>
+            </div>
+          </li>
+        )}
+      </div>
+    )
+  }
 }
 
 export default ActionList;
