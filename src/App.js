730e8379d2512f85c03f1c4e70808c40023f9c6b
Virginia Dooley <vcdooley@gmail.com>
2020-10-20 17:41:52 +0100

    fix list formatting

--- src/App.js
+++ src/App.js
@@ -17,19 +17,16 @@ function App() {
   }, [])
 
   return (
-    <div className="uk-list">
+    <div>
       <ul>
        {actions.map(item => 
-        <li key={item.Title}>
-          <div className="uk-card uk-card-secondary uk-card-body uk-width-1-2@m">
-            <div className="uk-card-badge uk-label">{item.badge}</div>
-            <p className="uk-card-title">Action: {item.Title}</p>
-            <a href="{item.Url}" className="uk-link-muted">{item.Url_title}</a>
-            <p>Date added: {item.date_added}</p>
-            <p>Deadline: {item.deadline}</p>
-            <p id="status">Complete?: {item.Status}</p>
-            <button className="uk-button uk-button-primary">COMPLETE</button>
-          </div>
+        <li key={item.Title} className="uk-card uk-card-secondary uk-card-body uk-width-1-2@m uk-margin-top uk-margin-bottom">
+          <div className="uk-card-badge uk-label">{item.badge}</div>
+          <p className="uk-card-title">Action: {item.Title}</p>
+          <a href="{item.Url}" className="uk-link-muted">{item.Url_title}</a>
+          <p>Date added: {item.date_added}</p>
+          <p>Deadline: {item.deadline}</p>
+          <button className="uk-button uk-button-primary">COMPLETE</button>
         </li>)}
       </ul>
     </div>
